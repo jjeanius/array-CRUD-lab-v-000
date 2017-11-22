e2627449387a6c9cf660445ea111ca231245f79b
Judy McGoldrick <jjeanius@verizon.net>
2017-11-22 02:25:52 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["red", "orange", "blue", "white"]
-  array << "yellow"
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
