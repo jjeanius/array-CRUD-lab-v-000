3e426143359c9b5801881725fdf535f7b44d032f
Judy McGoldrick <jjeanius@verizon.net>
2017-11-22 02:27:44 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["red", "orange", "blue", "white"]
   array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array.unshift("element")
 end
 
 def remove_element_from_end_of_array(array)
